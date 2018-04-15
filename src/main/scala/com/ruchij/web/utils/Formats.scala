e198aa459423840dc1677558f3a94d1605b213f3
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-04-15 20:31:34 +1000

    Adding the Joda time serializer

--- src/main/scala/com/ruchij/web/utils/Formats.scala
+++ src/main/scala/com/ruchij/web/utils/Formats.scala
@@ -1,11 +1,38 @@
 package com.ruchij.web.utils
 
 import com.ruchij.models.{ServiceInformation, Tag}
-import net.liftweb.json.{DefaultFormats, Extraction, JValue}
+import net.liftweb.json._
+import org.joda.time.DateTime
+
+import scala.util.Try
 
 object Formats
 {
-  implicit def defaultFormats: DefaultFormats = DefaultFormats
+  implicit def defaultFormats: Formats = DefaultFormats + JodaTimeSerializer
+
+  private object JodaDateTime
+  {
+    def unapply(jValue: JValue): Option[DateTime] =
+      jValue match {
+        case JString(dateTimeString) => Try(DateTime.parse(dateTimeString)).toOption
+        case _ => None
+      }
+  }
+
+  implicit object JodaTimeSerializer extends Serializer[DateTime]
+  {
+    private val typeClass: Class[DateTime] = classOf[DateTime]
+
+    override def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), DateTime] =
+    {
+      case (TypeInfo(`typeClass`, _), JodaDateTime(dateTime)) => dateTime
+    }
+
+    override def serialize(implicit format: Formats): PartialFunction[Any, JValue] =
+    {
+      case dateTime: DateTime => JString(dateTime.toString)
+    }
+  }
 
   implicit def serviceInfoJValue(serviceInformation: ServiceInformation): JValue =
     Extraction.decompose(serviceInformation)
